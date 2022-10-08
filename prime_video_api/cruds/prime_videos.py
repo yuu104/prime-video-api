8fbe67c144a4bf24dd81d90e88de9d61d2a3b2ee
yuu <yusei8171@gmail.com>
2022-10-08 18:06:53 +0900

    作品情報（available_button）をスクレイピングにより取得する際のHTMLタグのクラス名が変更していたため修正

--- prime_video_api/cruds/prime_videos.py
+++ prime_video_api/cruds/prime_videos.py
@@ -41,7 +41,7 @@ async def get_video_info(getVideoInfoDto: prime_videos_schema.GetVideoInfoDto) -
   driver.get(getVideoInfoDto.url)
   html = driver.page_source.encode('utf-8')
   soup = BeautifulSoup(html, 'html.parser')
-  available_button = soup.find('a', class_='_1ovr-S _15Ikr8 _3kM4Lo _14ydqK _2X_Irl _1ITy4O dv-signup-button')
+  available_button = soup.find('a', class_='SPqQmU _3RF4FN _1D7HW3 _1c8faI _3cSKlv _1ITy4O dv-signup-button')
   is_available = False if available_button is None else True
   leaving_schedule = soup.find('span', class_='_36qUej _1jE1N6')
   is_leaving_soon = False if leaving_schedule is None else True
