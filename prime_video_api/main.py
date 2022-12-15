7cbbd4da6bbddad689b223c4826b00ac2d4c1a2c
yuu <yusei8171@gmail.com>
2022-12-15 15:03:19 +0900

    CORS 対策

--- prime_video_api/main.py
+++ prime_video_api/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from  prime_video_api.routers import prime_videos
+from starlette.middleware.cors import CORSMiddleware # 追加
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(prime_videos.router)
